3acd332185d33588d8e8ee34f242d1f2905aa410
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-14 23:34:55 +0100

    footer skeleton updte

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-black flex flex-col bottom-0'>
-      <div className='border-white flex flex-col'>
+      <div className='border-white flex flex-col px-4'>
         <div className='text-brand'> 
           Erudition
         </div>
-        <div className='text-xs'>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate eaque atque perspiciatis quibusdam ea dignissimos placeat nemo! Magnam delectus, eveniet quae tempora nobis quod corrupti, omnis animi vitae, vel deleniti!
+        <div className='text-xs w-8'>
+          The Future of learning. Improving The way you learn
         </div>
       </div>
-      <div className='flex w-full bg-black-100 p-1 justify-between'>
+      <div className='flex w-full bg-black-100 p-4 justify-between'>
         <div>
           Social Links
         </div>
