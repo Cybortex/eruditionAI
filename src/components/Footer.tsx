a42257464a59ce47f96b8e2d99c7e054ab89d6b7
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-14 23:55:04 +0100

    footer skeleton complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-black flex flex-col bottom-0'>
+      <div className='flex justify-between'>
       <div className='border-white flex flex-col px-4 py-10'>
         <div className='text-brand text-xl'> 
           Erudition
@@ -10,6 +11,23 @@ const Footer = () => {
         <div className='text-xs w-18'>
           The Future of learning. Improving The way you learn
         </div>
+      </div>
+      <div className='border-white flex flex-col px-4 py-10'>
+        <div className='text-brand text-xl'> 
+          Erudition
+        </div>
+        <div className='text-xs w-18'>
+          The Future of learning. Improving The way you learn
+        </div>
+      </div><div className='border-white flex flex-col px-4 py-10'>
+        <div className='text-brand text-xl'> 
+          Erudition
+        </div>
+        <div className='text-xs w-18'>
+          The Future of learning. Improving The way you learn
+        </div>
+      </div>
+
       </div>
       <div className='flex w-full bg-black-100 p-4 justify-between'>
         <div>
