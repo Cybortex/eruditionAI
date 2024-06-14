2edd1694a95cb493bd8dab79508588c7653165fd
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-14 23:45:12 +0100

    footer updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-black flex flex-col bottom-0'>
-      <div className='border-white flex flex-col px-4'>
-        <div className='text-brand'> 
+      <div className='border-white flex flex-col px-4 py-10'>
+        <div className='text-brand text-xl'> 
           Erudition
         </div>
-        <div className='text-xs w-8'>
+        <div className='text-xs w-18'>
           The Future of learning. Improving The way you learn
         </div>
       </div>
