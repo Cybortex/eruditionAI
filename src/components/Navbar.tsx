41327ff265f8470cdf05013749b9697a184927e6
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-14 13:07:50 +0100

    completing navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Link } from "next/link";
 import Image from 'next/image'
 import { navItems } from "@/data";
-import { FaBars, FaXmark } from "react-icons/fa";
+import { FaBars, FaMixer} from "react-icons/fa";
 
 const Navbar = () => {
 
@@ -31,7 +30,7 @@ const Navbar = () => {
   return (
     <div>
 
-        <header className=' absolute top-0 flex z-50 p-5 w-full items-center justify-between'>
+        <header className=' absolute top-0 flex z-50 p-5 w-full items-center justify-between md:justify-evenly'>
             <Image 
                 src="/Logo.png"
                 width={50}
@@ -51,22 +50,22 @@ const Navbar = () => {
               <a href={`${nav.link}`}>{nav.name}</a>
             </li>
           ))}
-                </ul>
+          </ul>
 
-                <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <Image
-            src={toggle ? FaBars : FaXmark}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain'
+          <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <div
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
-          />
+          >
+              {toggle ?  <FaMixer size={28} /> : <FaBars size={28} />}
+          </div>
 
           <div
             className={`${
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+            <ul className='list-none flex justify-center items-start flex-1 flex-col gap-4'>
               {navItems.map((nav) => (
                 <li
                   key={nav.link}
