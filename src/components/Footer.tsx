b5f87439cd6db40aa14e1f28ee0ad83008f56a1b
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-14 23:27:53 +0100

    footer skeleton

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-black bottom-0 h-[5vh]'>
-      <div className='border-white '>
+    <div className='bg-black flex flex-col bottom-0'>
+      <div className='border-white flex flex-col'>
         <div className='text-brand'> 
           Erudition
         </div>
+        <div className='text-xs'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate eaque atque perspiciatis quibusdam ea dignissimos placeat nemo! Magnam delectus, eveniet quae tempora nobis quod corrupti, omnis animi vitae, vel deleniti!
+        </div>
       </div>
-      
+      <div className='flex w-full bg-black-100 p-1 justify-between'>
+        <div>
+          Social Links
+        </div>
+        <div>
+          Copyright Cybortex Inc
+        </div>
+      </div>
     </div>
   )
 }
