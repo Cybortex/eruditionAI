51d3b6362a844333761e71ffdbcee3c4a7cd84a7
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-15 01:11:05 +0100

    Footer Main layout complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
+import { FaGithub, FaTwitter, FaInstagram, FaYoutube, FaCopyright } from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <div className='bg-black flex flex-col bottom-0'>
-      <div className='flex justify-between'>
+      <div className='flex justify-between border-y border-white-100 '>
       <div className='border-white flex flex-col px-4 py-10'>
-        <div className='text-brand text-xl'> 
-          Erudition
+        <div className='text-brand text-xl font-bold uppercase'> 
+          Erudition AI
         </div>
         <div className='text-xs w-18'>
           The Future of learning. Improving The way you learn
         </div>
       </div>
       <div className='border-white flex flex-col px-4 py-10'>
-        <div className='text-brand text-xl'> 
+        <div className='text-brand text-xl font-bold uppercase'> 
           Important links
         </div>
         <div className='text-xs w-18'>
           The Future of learning. Improving The way you learn
         </div>
-      </div><div className='border-white flex flex-col px-4 py-10'>
-        <div className='text-brand text-xl'> 
+      </div><div className='flex flex-col px-4 py-10'>
+        <div className='text-brand text-xl font-bold uppercase'> 
           Important Links
         </div>
         <div className='text-xs w-18'>
@@ -29,12 +30,15 @@ const Footer = () => {
       </div>
 
       </div>
-      <div className='flex w-full bg-black-100 p-4 justify-between'>
-        <div>
-          Social Links
+      <div className='flex w-full bg-black-100 p-3 justify-between'>
+        <div className='flex gap-10'>
+        <FaInstagram />
+        <FaGithub />
+        <FaYoutube />
+        <FaTwitter />
         </div>
-        <div>
-          Copyright Cybortex Inc
+        <div className='flex items-center gap-2'>
+          Copyright <FaCopyright /> Cybortex Inc
         </div>
       </div>
     </div>
