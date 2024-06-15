b8aa4297f8a2354e7aab2561f40911e23fd9bb19
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-15 23:48:14 +0100

    TeamCArd

--- src/components/TeamCards.tsx
+++ src/components/TeamCards.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const TeamCards = () => {
   return (
-    <section className='h-full'>
+    <section className='h-full mt-10'>
         <div className='container mx-auto h-full'>
             <div className="flex flex-col items-center justify-between xl:flex-row xl:pt-8 xl:pb-24">
 
                 <div className="text-center xl:text-left">
-                    <span className='text-lg'>Computer Science Student</span>
-                    <h1 className='text-[36px] xl:text-[64px] leading-[1.1]font-semibold'>
+                    <span className='text-md'>Computer Science Student</span>
+                    <h1 className='text-[28px] xl:text-[36px] leading-[1.1]font-semibold'>
                         I&apos;m Folorunsho Olugboji
                     </h1>
                     <p className='max-w-[500px]mb-9 text-white/80'>Ambitious young entreprnuer and writer...</p>
