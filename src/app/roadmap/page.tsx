57afe527ba18722340202b7a534528d2a99c9002
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-16 23:38:05 +0100

    Roadmap

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Roadmap() {
   return (
-    <div>Roadmap</div>
+    <div>
+
+      <Roadmap />
+
+    </div>
   )
 }
 
---
+++ src/components/Roadmap.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { roadmap } from '@/data'
+
+const RoadmapCard = ({roadmap}) => {
+
+}
+
+const Roadmap = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Roadmap
