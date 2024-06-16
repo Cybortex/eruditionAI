87f393de25b69ede56763367d7dea0ec3c182278
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-16 23:55:01 +0100

    framer motion in roadmap

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { roadmap } from '@/data'
 import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component"
+import {motion} from 'framer-motion'
 
 const RoadmapCard = ({roadmap}) => {
 
