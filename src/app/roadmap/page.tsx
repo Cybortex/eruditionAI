ac75375a8b52a6d2daa45ef06c34d6f700d625f3
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-17 22:43:26 +0100

    team page removed

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,10 +1,26 @@
+import { roadmap } from '@/data'
 import React from 'react'
+import { VerticalTimeline } from 'react-vertical-timeline-component'
+import { motion } from 'framer-motion'
+
+import 'react-vertical-timeline-component/style.min.css'
+
+
+const RoadmapCard = ({ roadmap }) => {
+
+}
 
 function Roadmap() {
   return (
-    <div>
-      hello
-      
+    <div className='flex flex-cool mt-20'>
+
+      <VerticalTimeline>
+        {roadmap.map((roadmap, index) => (
+          <RoadmapCard key={index} roadmap={roadmap}/>
+        )) }
+        
+      </VerticalTimeline>      
+
     </div>
   )
 }
