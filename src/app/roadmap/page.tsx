9605bf6467adc9021f24f9d872346ebb428f4685
Folorunsho Olugboji <folugbojifx@gmail.com>
2024-06-18 11:03:29 +0100

    fixing roadmap page using daisy ui

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,28 +1,51 @@
-import { roadmap } from '@/data'
 import React from 'react'
-import { VerticalTimeline } from 'react-vertical-timeline-component'
-import { motion } from 'framer-motion'
 
-import 'react-vertical-timeline-component/style.min.css'
-
-
-const RoadmapCard = ({ roadmap }) => {
-
-}
-
-function Roadmap() {
+const page = () => {
   return (
-    <div className='flex flex-cool mt-20'>
-
-      <VerticalTimeline>
-        {roadmap.map((roadmap, index) => (
-          <RoadmapCard key={index} roadmap={roadmap}/>
-        )) }
-        
-      </VerticalTimeline>      
-
+    <div className='flex justify-center items-center flex-col overflow-hidden  sm:px-10 px-5 py-[10rem]'>
+      <ul className="timeline timeline-vertical">
+  <li>
+    <div className="timeline-start timeline-box">First Macintosh computer</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <hr/>
+  </li>
+  <li>
+    <hr/>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end timeline-box">iMac</div>
+    <hr/>
+  </li>
+  <li>
+    <hr/>
+    <div className="timeline-start timeline-box">iPod</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <hr/>
+  </li>
+  <li>
+    <hr/>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end timeline-box">iPhone</div>
+    <hr/>
+  </li>
+  <li>
+    <hr/>
+    <div className="timeline-start timeline-box">Apple Watch</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+  </li>
+</ul>
+      
     </div>
   )
 }
 
-export default Roadmap
+export default page
